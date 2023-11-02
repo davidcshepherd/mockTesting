0fca2212b9e31749c89274627776b6ba24893293
davidcshepherd <davidshepherd@gmail.com>
2023-11-02 16:13:20 -0500

    Update MyClassForTestingTest.java

--- src/test/java/delft/MyClassForTestingTest.java
+++ src/test/java/delft/MyClassForTestingTest.java
@@ -2,23 +2,9 @@ package delft;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 public class MyClassForTestingTest {
 
     @Test
     public void getInput() {
-        BufferedReader br = mock(BufferedReader.class);
-        try {
-            when(br.readLine()).thenReturn("1");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        MyClassForTesting myClassForTesting = new MyClassForTesting(br);
-        assert(1==myClassForTesting.getInput());
     }
 }
