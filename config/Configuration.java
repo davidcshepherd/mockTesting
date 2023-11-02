838ee8b337c3055024b4c070136e723f2430fb46
davidcshepherd <davidshepherd@gmail.com>
2023-11-02 16:10:04 -0500

    Update Configuration.java

--- config/Configuration.java
+++ config/Configuration.java
@@ -21,7 +21,7 @@ public class Configuration extends RunConfiguration {
 
     @Override
     public List<String> classesUnderTest() {
-        return List.of("delft.MyClassForTestingTest");
+        return List.of("delft.MyClassForTesting");
     }
 
     @Override
