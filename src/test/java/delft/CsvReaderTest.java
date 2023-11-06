d03460c4e9302182835b2d1178ab592757129670
davidcshepherd <davidshepherd@gmail.com>
2023-11-06 13:37:27 -0600

    csv

---
+++ src/test/java/delft/CsvReaderTest.java
@@ -0,0 +1,16 @@
+package delft;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CsvReaderTest {
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void detectColumnTypes() {
+    }
+}
